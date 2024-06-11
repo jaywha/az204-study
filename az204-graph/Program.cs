be4fa9a2be867957b03de7afb5d6ff2e21179489
jaywha <jayw685@gmail.com>
2024-06-11 13:50:05 -0500

    graph and service bus lessons done

---
+++ az204-graph/Program.cs
@@ -0,0 +1,72 @@
+﻿using Azure.Identity;
+using Microsoft.Graph;
+using Microsoft.Graph.Core;
+using Microsoft.Graph.Models;
+
+internal class Program
+{
+    private static async global::System.Threading.Tasks.Task Main(string[] args)
+    {
+        var scopes = new[] { "User.Read" };
+
+        // Multi-tenant apps can use "common",
+        // single-tenant apps must use the tenant ID from the Azure portal
+        var tenantId = "27882565-f13a-4eda-9153-5786e2237502";
+
+        // Value from app registration
+        var clientId = "8ab07d57-7ebe-496a-b2b3-788763d8b953";
+
+        // using Azure.Identity;
+        var options = new TokenCredentialOptions
+        {
+            AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+        };
+
+        // Callback function that receives the user prompt
+        // Prompt contains the generated device code that you must
+        // enter during the auth process in the browser
+        Func<DeviceCodeInfo, CancellationToken, Task> callback = (code, cancellation) =>
+        {
+            Console.WriteLine(code.Message);
+            return Task.FromResult(0);
+        };
+
+        // /dotnet/api/azure.identity.devicecodecredential
+        var deviceCodeCredential = new DeviceCodeCredential(
+            callback, tenantId, clientId, options);
+
+        var graphClient = new GraphServiceClient(deviceCodeCredential, scopes);
+
+        // GET https://graph.microsoft.com/v1.0/me
+
+        var user = await graphClient.Me
+            .GetAsync();
+
+        // GET https://graph.microsoft.com/v1.0/me/messages?$select=subject,sender&$filter=<some condition>&orderBy=receivedDateTime
+
+        var messages = await graphClient.Me.Messages.GetAsync() ?? new MessageCollectionResponse() { OdataCount = -1, Value = null };
+
+        if (messages.Value == null || messages.OdataCount == -1) throw new Exception();
+        
+        foreach(var message in messages.Value)
+        {            
+            Console.WriteLine(message.ToString());
+        }
+
+        // DELETE https://graph.microsoft.com/v1.0/me/messages/{message-id}
+
+        /*string messageId = "";
+        var message = await graphClient.Me.Messages[messageId]
+            .Request()
+            .DeleteAsync();*/
+
+        // POST https://graph.microsoft.com/v1.0/me/calendars
+
+        var calendar = new Calendar
+        {
+            Name = "Volunteer"
+        };
+
+        var newCalendar = await graphClient.Me.Calendars.PostAsync(calendar);
+    }
+}
---
+++ az204-svcbus/Program.cs
@@ -0,0 +1,112 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+
+namespace az204_svcbus
+{
+    public class Driver
+    {
+        public static void Main(string[] args)
+        {
+            // connection string to your Service Bus namespace
+            string connectionString = args[0];
+
+            // name of your Service Bus topic
+            string queueName = "az204-queue";
+
+            // the client that owns the connection and can be used to create senders and receivers
+            ServiceBusClient client;
+
+            // the sender used to publish messages to the queue
+            ServiceBusSender sender;
+
+            // Create the clients that we'll use for sending and processing messages.
+            client = new ServiceBusClient(connectionString);
+            sender = client.CreateSender(queueName);
+
+            // create a batch 
+            using ServiceBusMessageBatch messageBatch = WaitOn<ServiceBusMessageBatch>(sender.CreateMessageBatchAsync()).Result;
+
+            for (int i = 1; i <= 3; i++)
+            {
+                // try adding a message to the batch
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+                {
+                    // if an exception occurs
+                    throw new Exception($"Exception {i} has occurred.");
+                }
+            }
+
+            try
+            {
+                // Use the producer client to send the batch of messages to the Service Bus queue
+                sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of three messages has been published to the queue.");
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                WaitOn(sender.DisposeAsync());
+                WaitOn(client.DisposeAsync());
+            }
+
+            Console.WriteLine("Follow the directions in the exercise to review the results in the Azure portal.");
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+
+            // Test Receive Messages
+            ServiceBusProcessor processor;
+            client = new ServiceBusClient(connectionString);
+
+            // create a processor that we can use to process the messages
+            processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+            try
+            {
+                // add handler to process messages
+                processor.ProcessMessageAsync += MessageHandler;
+
+                // add handler to process any errors
+                processor.ProcessErrorAsync += ErrorHandler;
+
+                // start processing 
+                processor.StartProcessingAsync();
+
+                Console.WriteLine("Wait for a minute and then press any key to end the processing");
+                Console.ReadKey();
+
+                // stop processing 
+                Console.WriteLine("\nStopping the receiver...");
+                processor.StopProcessingAsync();
+                Console.WriteLine("Stopped receiving messages");
+            }
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                WaitOn(processor.DisposeAsync());
+                WaitOn(client.DisposeAsync());
+            }
+
+            // handle received messages
+            async Task MessageHandler(ProcessMessageEventArgs args)
+            {
+                string body = args.Message.Body.ToString();
+                Console.WriteLine($"Received: {body}");
+
+                // complete the message. messages is deleted from the queue. 
+                await args.CompleteMessageAsync(args.Message);
+            }
+
+            // handle any errors when receiving messages
+            Task ErrorHandler(ProcessErrorEventArgs args)
+            {
+                Console.WriteLine(args.Exception.ToString());
+                return Task.CompletedTask;
+            }
+        }
+
+        private static async Task<T> WaitOn<T>(ValueTask<T> valueTask) => await valueTask;
+        private static async void WaitOn(ValueTask valueTask) => await valueTask;
+    }
+}
